c0d08a8cd3052f3c3b8d181b1a97b3a1d457d6c4
vorillaz <theodorevorillas@gmail.com>
2022-11-05 18:59:09 +0200

    feat(web): this is a feat

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,6 +4,12 @@ export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
+      <h1>Docs</h1>
+      <h1>Docs</h1>
+      <h1>Docs</h1>
+      <h1>Docs</h1>
+      <h1>Docs</h1>
+      <h1>Docs</h1>
       <Button />
     </div>
   );
