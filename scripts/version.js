0baff8afdb3b9325f5fc348a0da343073876aff6
vorillaz <theodorevorillas@gmail.com>
2022-11-05 20:28:30 +0200

    fix: another try

--- scripts/version.js
+++ scripts/version.js
@@ -12,14 +12,13 @@ const semanticRelease = require("semantic-release");
       plugins: ["@semantic-release/commit-analyzer"],
     });
     if (result) {
-      console.log(result);
       console.log("Next release version:", result.nextRelease.version);
       console.log("Next release gitTag:", result.nextRelease.gitTag);
       //   Cause the release version should be passed across steps in the github workflow we need to write it to a file
-      fs.writeFileSync("release-version.txt", result.nextRelease.version);
-      fs.writeFileSync("release-gitTag.txt", result.nextRelease.gitTag);
+      fs.writeFileSync("next-release-version.txt", result.nextRelease.version);
+      fs.writeFileSync("next-release-gitTag.txt", result.nextRelease.gitTag);
 
-      //   move on
+      //   move on and skip the rest of the steps
     } else {
       console.log("No release");
       process.exitCode = 0;
